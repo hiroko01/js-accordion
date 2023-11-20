1ca9f45ddace1d0dcca5a4ed7e0860aa94852c9d
hiroko01 <hirokosakamoto31@gmail.com>
2023-11-20 23:12:12 +0900

    リファクタリング完了

--- accordion.js
+++ accordion.js
@@ -3,24 +3,20 @@
     const $trigger = $elm.getElementsByTagName('a');
 
     const triggerLen = $trigger.length;
-    //カウンター let index = 0;
+
     let index = 0;
     while (index < triggerLen) {
-        $trigger[index].addEventListener('click',(e) => clickHandler (e) );
-        index ++;
+        $trigger[index].addEventListener('click',(e) => clickHandler(e));
+        index++;
     }
 
-    //クリックハンドラーの中身はここから書いていきます↓
     //クリックしたら実行される処理
     const clickHandler =  (e) => {
         e.preventDefault();
 
         const $target = e.currentTarget;
+        const $content = $target.nextElementSibling;
 
-//「nextElementSibling」は＄triggerで<a>タブを取ってる、次の要素を取ってくる
-//今回は「<div class="accordion-contents">」が「nextElementSibling」に当てはまる
-
-        const $content = $trigger[0].nextElementSibling;
         if($content.style.display === 'block'){
             $content.style.display = 'none';
         } else {
@@ -28,3 +24,6 @@
         }
     };
 })();
+
+
+
