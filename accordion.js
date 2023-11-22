3ffc197f7bd6af97cf42902695d60a2291ace278
hiroko01 <hirokosakamoto31@gmail.com>
2023-11-22 22:29:38 +0900

    クラスとインスタンス完成

--- accordion.js
+++ accordion.js
@@ -1,28 +1,59 @@
 (() => {
-    const $elm =document.querySelector('#js-accordion');
-    const $trigger = $elm.getElementsByTagName('a');
+    class Accordion {
+        //初期化 最初に実行したいもの入れる
+        constructor(obj){
 
-    const triggerLen = $trigger.length;
+            console.log('obj', obj.hookName);
 
-    let index = 0;
-    while (index < triggerLen) {
-        $trigger[index].addEventListener('click',(e) => clickHandler(e));
-        index++;
+            const $elm =document.querySelector(obj.hookName);
+            const $trigger = $elm.getElementsByTagName(obj.tagName);
+
+            const triggerLen = $trigger.length;
+            let index = 0;
+            while (index < triggerLen) {
+                $trigger[index].addEventListener('click',(e) => this.clickHandler(e));
+                index++;
+            }
+            this.actionU();
+        }
+
+        actionU(){
+            console.log('Hello World!');
+        };
+
+
+        //クリックしたら実行される処理　classの中に書く命令はconstいらない
+        clickHandler(e){
+            e.preventDefault();
+
+            const $target = e.currentTarget;
+            const $content = $target.nextElementSibling;
+
+            if($content.style.display === 'block'){
+                $content.style.display = 'none';
+            } else {
+                $content.style.display = 'block';
+            }
+        }
     }
 
-    //クリックしたら実行される処理
-    const clickHandler =  (e) => {
-        e.preventDefault();
+    const fuckingAccordion = new Accordion({
+        hookName: '#js-faq',
+        tagName:'p'
+    });
 
-        const $target = e.currentTarget;
-        const $content = $target.nextElementSibling;
 
-        if($content.style.display === 'block'){
-            $content.style.display = 'none';
-        } else {
-            $content.style.display = 'block';
-        }
-    };
+    const dummyAccordion = new Accordion({
+        hookName: '#js-accordion',
+        tagName:'a'
+    });
+
+    const miniAccordion = new Accordion({
+        hookName: '#js-accordion-mini',
+        tagName:'dt'
+    });
+
+
 })();
 
 
